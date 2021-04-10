e11a8d761ae46673bf3776ca1ac182ef7bf75295
Alireza Alibeiki <alibeikialireza@gmail.com>
2021-04-10 18:35:42 +0430

    000121 optimize years prop

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -749,15 +749,11 @@ export default {
       return attrs;
     },
     years() {
-      let years = [];
-      for (
-        let startYear = this.fromDate.year();
-        startYear <= this.toDate.year();
-        startYear++
-      ) {
-        years.push(startYear);
-      }
-      return years;
+      let start = this.fromDate.year();
+      const end = this.toDate.year();
+      return Array(end - start + 1)
+        .fill(null)
+        .map(() => start++);
     },
     monthDays() {
       let months = [];
@@ -855,7 +851,7 @@ export default {
         const scale = column / (this.mode == "single" ? 1 : 2);
         this.$refs.root.style.setProperty(
           "--time-scale",
-          scale >= 1 ? scale : 1
+          scale > 1 ? scale : 1
         );
       }
       return column;
