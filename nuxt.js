e1659af31ad77d6952b7cfbbf551571191d33a76
Alireza Alibeiki <alibeikialireza@gmail.com>
2022-02-10 09:17:29 +0330

    fix nuxt path of PersianDate

--- nuxt.js
+++ nuxt.js
@@ -1,14 +1,14 @@
-import { join } from 'path'
+import { join } from "path";
 
 export default function (option) {
-  if (option['PersianDate']) {
-    this.addModule('@alireza-ab/persian-date/lib/nuxt')
-  }
-  this.nuxt.hook('components:dirs', (dirs) => {
-    dirs.push({
-      path: join(__dirname, 'src/components'),
-    })
-  })
+	if (option["PersianDate"]) {
+		this.addModule("@alireza-ab/persian-date/nuxt");
+	}
+	this.nuxt.hook("components:dirs", (dirs) => {
+		dirs.push({
+			path: join(__dirname, "src/components"),
+		});
+	});
 }
 
-module.exports.meta = require('./package.json')
+module.exports.meta = require("./package.json");
